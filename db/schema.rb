66a79728d9bb376ab3460fdc4d7204088d718ad4
crazyma-nn <iwn001bim0@gmail.com>
2020-02-04 22:49:44 +0900

    ユーザーモデルの追加、登録・削除・編集機能の実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_24_132556) do
+ActiveRecord::Schema.define(version: 2020_02_04_123202) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "context"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
+    t.boolean "admin", default: false, null: false
   end
 
 end
